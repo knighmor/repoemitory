55fc608fdb52a8581042b77164aae65bf2ba53e1
Morgaine Knight <knighmor2020@gmail.com>
2026-07-15 16:25:31 -0400

    smaller workload today-- worked on setting up navigation bar, along with desktop layout. to be finished tomorrow.

---
+++ js/index.js
@@ -0,0 +1,4 @@
+
+// setting up the button to pull up and down the nav bar
+const navButton = document.getElementsByClassName("poem-nav-button");
+const navBar = document.getElementsByClassName("poem-nav");
