2e8fd11171e506d811adb6a78df93d713c45f4ce
Morgaine Knight <knighmor2020@gmail.com>
2026-07-18 19:00:04 -0400

    changed most of flex properties to grid, because i hate columns-- also started up express dev/prod server. also some reorganizing. yay.

--- js/index.js
+++
@@ -1,4 +0,0 @@
-
-// setting up the button to pull up and down the nav bar
-const navButton = document.getElementsByClassName("poem-nav-button");
-const navBar = document.getElementsByClassName("poem-nav");
