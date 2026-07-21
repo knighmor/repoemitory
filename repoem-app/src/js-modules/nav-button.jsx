3d3b512cc4b50db1cb8d96bc9b38cce73e587134
Morgaine Knight <knighmor2020@gmail.com>
2026-07-21 17:48:10 -0400

    forgot stuff in the last commit releating to certain files, whoops

--- repoem-app/src/js-modules/nav-button.jsx
+++ repoem-app/src/js-modules/nav-button.jsx
@@ -3,4 +3,16 @@ import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const navButton = document.getElementsByClassName("poem-nav-button");
+navButton.onClick
+
+export default function navmenuDrop() {
+    const [dropped, setDropped] = useState(false);
+
+    function handleClick() {
+        if (e.target.click) {
+            p
+        }
+    }
+}
+
 console.log(navButton);
